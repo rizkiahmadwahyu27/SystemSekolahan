d08656989f0df6997d750b6e97a4c9313bc50485
Rizki Ahmad Wahyu <rizkiahmadwahyu27@gmail.com>
2025-11-18 12:03:57 +0700

    hapus code tidak penting

--- resources/views/siswa/lap_bulanan_siswa.blade.php
+++ resources/views/siswa/lap_bulanan_siswa.blade.php
@@ -2,13 +2,6 @@
     <div class="w-full h-16 bg-slate-100 px-3 py-2">
         <div class="flex justify-between items-center">
             <div class="flex justify-center items-center mr-2">
-                <div class="ml-5">
-                    <a href="{{route('export_absensi')}}">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 md:size-8 text-slate-400 hover:text-slate-600">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-                        </svg>
-                    </a>
-                </div>
                 @if (Auth::user()->level != 'siswa')
                     <div class="ml-5">
                         <button type="button" x-data @click="window.dispatchEvent(new CustomEvent('open-modal', { detail: 'filter_absen_bulanan_siswa' }))">
